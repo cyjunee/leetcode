5220ec5780f32892ea7b9fcaefbea98a87f77fcd
Yeongjun Cho <71057287+cyjunee@users.noreply.github.com>
2022-11-23 21:56:05 +0900

    Time: 112 ms (84.35%), Space: 20.8 MB (30.00%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,21 +1,8 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        if n == 0:
-            return [0]
+        out = [0]
         
-        out = [0 for i in range(n+1)]
-        out[1] = 1
-        
-        idx = 2
-        period = 2
-        
-        while idx <= n:
-            for i in range(period):
-                if idx + i > n:
-                    break
-                out[idx+i] = out[i] + 1
+        for i in range(1,n+1):
+            out.append(i % 2 + out[i>>1])
             
-            idx += period
-            period *= 2
-        
         return out
